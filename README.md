d63714d06bc13670dfcfd675fdebd6df0735a40e
Michal Polovka <mpolovka@redhat.com>
2019-02-11 10:30:26 +0100

    Tested on Fedora

--- README.md
+++ README.md
@@ -3,11 +3,10 @@
 Install with:
 
 ```bash
-git clone https://github.com/fabianbl/lenovo_professional_wireless_keyboard.git ~/lenovo_professional_wireless_keyboard
+git clone https://github.com/fabianbl/lenovo_professional_wireless_keyboard.git
 cd ~/lenovo_professional_wireless_keyboard
 ./install.sh
-rm -rf ~/lenovo_professional_wireless_keyboard
 ```
 
-Kernel: 4.10
-Ubuntu: 16.04
+Kernel: 4.20
+Fedora 28
--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-gcc -o kbdusb kbdusb.c &\
-sudo cp kbdusb /usr/local/bin/. &\
-sudo cp lenovo_keyboard.sh /usr/local/bin/. &\
-sudo cp lenovo_keyboard.service /etc/systemd/system/. &\
-sudo systemctl enable lenovo_keyboard.service &\
-sudo systemctl start lenovo_keyboard.service
+gcc -o kbdusb kbdusb.c  &&
+sudo cp kbdusb /usr/local/bin/.  && 
+sudo cp lenovo_keyboard.sh /usr/local/bin/. &&
+sudo cp lenovo_keyboard.service /etc/systemd/system/. &&
+sudo systemctl enable lenovo_keyboard.service &&
+sudo systemctl start lenovo_keyboard.service &&
+systemctl status lenovo_keyboard.service
